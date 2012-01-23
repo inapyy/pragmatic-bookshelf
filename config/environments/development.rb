7e667a6f3feb665365f1cf44d6d5881f2140b26b
Kei Inagawa <k.inaga1028@gmail.com>
2012-01-23 23:35:11 +0900

    initial commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,16 @@
 Depot::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    address:         "smtp.gmail.com",
+    port:            587,
+    domain:          "domain.of.sender.net",
+    authentication:  "plain",
+    user_name:       "ikewaganai",
+    password:        "keikeiinaina",
+    enable_starttls_auto: true
+  }
 
   # In the development environment your application's code is reloaded on
   # every request.  This slows down response time but is perfect for development
