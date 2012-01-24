b3df7fbc0b43f901b75369309848343a03b5f546
Kei Inagawa <k.inaga1028@gmail.com>
2012-01-25 01:07:32 +0900

    first commit

---
+++ app/mailers/order_notifier.rb
@@ -0,0 +1,38 @@
+class OrderNotifier < ActionMailer::Base
+  default from: 'Sam Ruby<depot@example.com>'
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.received.subject
+  #
+  def received
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.shipped.subject
+  #
+  def shipped
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+  
+  def received(order)
+    @order = order
+    
+    mail to: order.email, subject: 'Pragmatic Store Order Confirmation'
+  end
+  
+  def shipped(order)
+    @order = order
+    
+    mail to: order.email, subject: 'Pragmatic Store Order Shipped'
+  end
+  
+end
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+require 'bcrypt'
+
+class User < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+  has_secure_password
+end
